c44d2ba863ea5480c79f725a4e9b2bb126b940f3
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-01 21:16:07 +0530

    .

---
+++ 19-Binary Tree/BFS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void BFS(Node* root) {
+    if (root == NULL) return;
+
+    queue<Node*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* curr = q.front();
+        q.pop();
+
+        cout << curr->data << " ";
+
+        if (curr->left) q.push(curr->left);
+
+        if (curr->right) q.push(curr->right);
+    }
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "BFS: ";
+    BFS(root);
+}
